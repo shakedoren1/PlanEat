6e6c9a296c65febf515a3e24e162687a810dca71
Shaked <shaked.oren@post.runi.ac.il>
2023-04-17 00:50:01 +0300

    bottom bar into mainActivity

--- app/src/main/java/com/example/planeatapp/MainActivity.java
+++ app/src/main/java/com/example/planeatapp/MainActivity.java
@@ -8,12 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView; //NavBar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // setting the NavBar
+        bottomNavigationView = findViewById(R.id.BottomNavigationView);
+        bottomNavigationView.setBackground(null);
+
 
         Button nextButton = findViewById(R.id.nextButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
