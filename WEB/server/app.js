f7afe480ec143180d093bdf6a2f667cdf19935c1
Shaked <shaked.oren@post.runi.ac.il>
2023-05-08 23:08:43 +0300

    Working on the server

--- WEB/server/app.js
+++ WEB/server/app.js
@@ -10,20 +10,40 @@ mongoClient.connect(url, (err, db) => {
     if (err) {
         console.log("Eror while connecting mongo client.")
     } else {
-        const myDB = db.db('myDB')
-        const collection = myDB.collection('myTable') // >>>>>>Change the name acording to the tables
+        const myDB = db.db('planEatDB')
+        const collection = myDB.collection('eventTable')
 
-        app.post('/event', (req, res) => {
+        app.post('/newEvent', (req, res) => {
             const newEvent = {
+                ID: req.body.id,
                 title: req.body.title,
                 date: req.body.date,
-                time: req.body.date,
+                time: req.body.time,
                 place: req.body.place,
                 concept: req.body.concept,
                 number: req.body.number
             }
             collection.insertOne(newEvent, (err, result) => {
                 res.status(200).send()
+            })
+        })
+
+        app.post('/eventDetails', (req, res) => {
+            const query = { ID: req.body.id }
+            collection.findOne(query, (err, result) => {
+                if (result != null) {
+                    const eventToSend = {
+                        title: result.title,
+                        date: result.date,
+                        time: result.time,
+                        place: result.place,
+                        concept: result.concept,
+                        number: result.number
+                    }
+                    res.status(200).send(JSON.stringify(eventToSend))
+                } else {
+                    res.status(404).send()
+                }
             })
         })
     }
