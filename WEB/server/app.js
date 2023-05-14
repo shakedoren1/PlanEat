009edc4a1c9b6edda996741b7914fe7b8a9bd606
Shaked <shaked.oren@post.runi.ac.il>
2023-05-14 14:37:34 +0300

    Server updates

--- WEB/server/app.js
+++ WEB/server/app.js
@@ -4,6 +4,8 @@ const mongoClient = require('mongodb').mongoClient
 
 const url = "mongodb://localhost.27017"
 
+const eventID = "0"
+
 app.use (express.json())
 
 mongoClient.connect(url, (err, db) => {
@@ -13,9 +15,9 @@ mongoClient.connect(url, (err, db) => {
         const myDB = db.db('planEatDB')
         const collection = myDB.collection('eventTable')
 
+        // The call from the new event to insert a new event into the data base
         app.post('/newEvent', (req, res) => {
             const newEvent = {
-                ID: req.body.id,
                 title: req.body.title,
                 date: req.body.date,
                 time: req.body.time,
@@ -24,14 +26,24 @@ mongoClient.connect(url, (err, db) => {
                 number: req.body.number
             }
             collection.insertOne(newEvent, (err, result) => {
-                res.status(200).send()
-            })
+                if (err) {
+                    res.status(500).send("Error while inserting a new event");
+                } else {
+                    eventID = result.insertedId; // Retrieve the auto-incremented ID
+                    res.status(200).json(eventID); // Send the ID in the response
+                }
+            });
         })
 
+        // The call from the home page to receive the data on the event
         app.post('/eventDetails', (req, res) => {
-            const query = { ID: req.body.id }
+            eventID = req.body.eventID; // Retrieve the eventID from the request body
+            const query = { insertedId: eventID }; // Use the eventID to search for the event in the collection
+
             collection.findOne(query, (err, result) => {
-                if (result != null) {
+                if (err) {
+                    res.status(500).send('Error retrieving event details');
+                } else if (result) {
                     const eventToSend = {
                         title: result.title,
                         date: result.date,
@@ -39,13 +51,13 @@ mongoClient.connect(url, (err, db) => {
                         place: result.place,
                         concept: result.concept,
                         number: result.number
-                    }
-                    res.status(200).send(JSON.stringify(eventToSend))
+                    };
+                    res.status(200).json(eventToSend); // Send the event details in the response
                 } else {
-                    res.status(404).send()
+                    res.status(404).send('Event not found');
                 }
-            })
-        })
+            });
+        });
     }
 })
 
