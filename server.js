9d11b4dd9f3c3c3cef248ad1a3d7cc0f297ea74b
Rotem Maniv <rotemmaniv@gmail.com>
2023-06-01 11:54:19 +0300

    removed null fragments and activities form past versions

--- server.js
+++ server.js
@@ -1,30 +1,45 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-app.use(express.json());
 
-app.post('/message', async (req, res) => {
-  const prompt = req.body.prompt;
-  const maxTokens = req.body.maxTokens || 60;
-
-  try {
-    const response = await axios.post('https://api.openai.com/v4/engines/davinci-codex/completions', {
-      prompt: prompt,
-      max_tokens: maxTokens
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.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}`
-      }
-    });
-
-    const message = response.data.choices[0].text.trim();
-    res.json({ message: message });
-  } catch (error) {
-    res.json({ error: error.message });
-  }
+app.get('/test', async (req, res) => {
+  res.json({ message: "here" });
+  console.log("Shaked");
 });
 
-const port = process.env.PORT || 3000;
+app.get('/test2', async (req, res) => {
+  const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+    model: "gpt-3.5-turbo-0301",
+    messages: [
+      {
+        role: "user",  
+        content: `Hi, I'm planning a Italian Meal in a potluck style for 10 friends at My house. Can you create a list of things we need to bring please? Make sure the list contains appetizers, mains, sides, dessert, drinks and any other food or non-food items needed, not ingredients or partial dishes. Have only the names of things, have appropriate amounts and be thorough please. No intro and no outro or tips. Thank you!`
+      }
+    ]
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer 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`
+    }
+  });
+
+  console.log('Response:', response.data);
+  let message = response.data.choices[0].message.content;
+  console.log("msg=", message, response.data);
+
+  // upload to db
+  // let success = await uploadToDB(message)
+  return res.send({ answer: message });
+});
+
+app.post('/message', async (req, res) => {
+  const prompt = req.body;
+  console.log("body", req.body);
+  const maxTokens = req.body.maxTokens || 60;
+  console.log("in msg", prompt, maxTokens);
+});
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
