b4f273a2c9396640cb603dddf39512d0d6eb9eba
Shaked Oren <shaked.oren.1@gmail.com>
2023-06-04 18:05:03 +0300

    Website and server comunicating. problem with the confirmation sending

--- WEB/app.js
+++ WEB/app.js
@@ -5,3 +5,45 @@ const eventID = urlParams.get('eventID');
 // Set the placeholder value with the ID
 const placeholder = document.getElementById('event-id');
 placeholder.innerHTML += `<h4>The event id is: ${eventID}</h4>`;
+
+// Set the value of the hidden input to the eventID
+const eventIDInput = document.getElementById('eventID');
+eventIDInput.value = eventID;
+
+// Make an AJAX request to the server to get the event info by the ID
+const xhr1 = new XMLHttpRequest();
+xhr1.open('GET', `http://localhost:8080/eventInfo/${eventID}`, true);
+xhr1.onload = function () {
+  if (xhr1.status === 200) {
+    const eventData = JSON.parse(xhr1.responseText);
+    // Update the event title
+    const eventTitle = document.getElementById('event-title');
+    eventTitle.innerHTML += `<h4>Will we see you at the ${eventData.title} on ${eventData.date} at ${eventData.place}?</h4>`;
+  }
+};
+xhr1.send();
+
+// // Add an event listener to the submit button
+// document.getElementById('btn').addEventListener('click', function(event) {
+//     event.preventDefault(); // Prevent the default form submission behavior
+
+//     // Make an AJAX request to the server to insert confirmation when submiting the form
+//     const xhr2 = new XMLHttpRequest();
+//     xhr2.open('POST', 'http://localhost:8080/confirmation', true);
+//     xhr2.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//     xhr2.onload = function () {
+//     if (xhr2.status === 200) {
+//         const response = JSON.parse(xhr2.responseText);
+//         // Display a toast notification
+//         Toastify({
+//             text: response.message,
+//             duration: 3000, // Toast duration in milliseconds (3 seconds)
+//             gravity: 'top', // Position the toast at the top of the screen
+//             close: true // Show a close button on the toast
+//             }).showToast();
+//         // Reset the form
+//         document.querySelector('.contact-form').reset(); 
+//     }
+//     };
+//     xhr2.send(new FormData(document.querySelector('.contact-form')));
+// });
--- WEB/server/server.js
+++ WEB/server/server.js
@@ -4,33 +4,36 @@ const { MongoClient } = require('mongodb')
 const { ObjectId } = require('mongodb');
 const url = 'mongodb://127.0.0.1:27017';
 const databaseName = 'PlanEat';
-const collectionName = 'eventsInfo';
+let collectionName = '';
 const client = new MongoClient(url);
 
 app.use(express.json());
 
-// A function that gets an event and insert it to the data base
-async function insertEvent(event) {
+app.use(express.static(__dirname + '/..'));
+
+// A function that gets an item and insert it to the collection
+async function insertItem(item, colName) {
   let result = await client.connect();
   db = result.db(databaseName);
-  collection = db.collection(collectionName);
-  const insertResult = await collection.insertOne(event);
+  collection = db.collection(colName);
+  const insertResult = await collection.insertOne(item);
   return insertResult;
 }
 
 // A function to retrieve event by ID from the database
-async function getEventById(eventId) {
+async function getEventById(eventId, colName) {
   let result = await client.connect();
   db = result.db(databaseName);
-  collection = db.collection(collectionName);
+  collection = db.collection(colName);
   const event = await collection.findOne({ _id: new ObjectId(eventId) });
   return event;
 }
 
-// The call from the new event to insert a new event into the database
+// The call from the creatEvent to insert a new event into the database
 app.post('/newEvent', (req, res) => {
   console.log('Entered /newEvent'); // for debug
 
+  collectionName = 'eventsInfo'
   const newEvent = {
     title: req.body.title,
     date: req.body.date,
@@ -43,7 +46,7 @@ app.post('/newEvent', (req, res) => {
 
   (async () => {
     try {
-      const response = await insertEvent(newEvent);
+      const response = await insertItem(newEvent, collectionName);
       console.log(response); // for debug
       res.status(200).json(response); // Send the responses of the insertion
     } catch (error) {
@@ -54,21 +57,47 @@ app.post('/newEvent', (req, res) => {
 
 });
 
-// The call from the home page to to get an event info based on an id from the database
+// The call to get an event info based on an id from the database
 app.get('/eventInfo/:id', async (req, res) => {
   console.log('Entered /eventInfo'); // for debug
 
+  collectionName = 'eventsInfo'
   const eventId = req.params.id;
   console.log(eventId); // for debug
 
   try {
-    const event = await getEventById(eventId);
+    const event = await getEventById(eventId, collectionName);
     console.log(event); // for debug
     res.status(200).json(event);
   } catch (error) {
     console.error(error); // for debug
     res.status(500).json({ error: 'Failed to retrieve event info' });
   }
+});
+
+// The call from the website to insert a confirmation into the database
+app.post('/confirmation', express.urlencoded({ extended: true }), (req, res) => {
+  console.log('Entered /confirmation'); // for debug
+
+  collectionName = 'confirmations'
+  const newconfirmation = {
+    eventID: req.body.eventID,
+    name: req.body.name,
+    option: req.body.option,
+  };
+  console.log(newconfirmation); // for debug
+
+  (async () => {
+    try {
+      const response = await insertItem(newconfirmation, collectionName);
+      console.log(response); // for debug
+      res.status(200).json({ message: 'Confirmation received!' }); // Send the responses of the insertion
+    } catch (error) {
+      console.error(error); // for debug
+      res.status(500).json({ error: 'Failed to insert confirmation' });
+    }
+  })();
+
 });
 
 app.listen(8080, () => {
