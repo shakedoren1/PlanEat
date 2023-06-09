e4461dcf64906eb1b48374e1294a21fec569cca0
Rotem Maniv <rotemmaniv@gmail.com>
2023-06-09 16:16:28 +0300

    added first notification and user info

--- ChatServer/server.js
+++ ChatServer/server.js
@@ -6,73 +6,143 @@ app.use(bodyParser.json());
 const { MongoClient, ObjectId } = require('mongodb');
 const url = 'mongodb+srv://PlanEatList:PlanEat123@planeat.selzkm5.mongodb.net/?retryWrites=true&w=majority';
 const databaseName = 'PlanEat';
-const collectionName = 'Ingredients';
+let collectionName = '';
 const client = new MongoClient(url);
+let prompt = '';
 
-app.get('/test', async (req, res) => {
-  res.json({ message: "here" });
-  console.log("Shaked");
-});
+app.use(express.json());
+// Allowing to run the website on the server
+app.use(express.static(__dirname + '/..'));
 
-app.get('/message', async (req, res) => {
-  const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-    model: "gpt-3.5-turbo-0301",
-    messages: [
-      {
-        role: "user",  
-        content: `Hi, I'm planning a Italian Meal in a potluck style for 10 friends at My house. Can you create a list of things we need to bring please? Make sure the list contains appetizers, mains, sides, dessert, drinks and any other food or non-food items needed, not ingredients or partial dishes. Have only the names of things, have appropriate amounts and be thorough please. No intro and no outro or tips. Thank you! Please return this as a JSON file where each item has an amount and an item name.`
-      }
-    ]
-  }, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer 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`
-    }
-  });
-
-  console.log('Response:', response.data);
-  let message = response.data.choices[0].message.content;
-
-  // upload to db
-  const newList = {
-    content: message
-  };
-  console.log(newList); // for debug
-
-  (async () => {
-    try {
-      const response = await insertList(newList);
-      console.log(response); // for debug
-      res.status(200).json(response); // Send the responses of the insertion
-    } catch (error) {
-      console.error(error); // for debug
-      res.status(500).json({ error: 'Failed to insert ingerdient list' });
-    }
-  })();
-});
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
-
-// A function that gets an event and insert it to the data base
-async function insertList(list) {
+// A function that gets an item and insert it to the collection
+async function insertItem(item, colName) {
   let result = await client.connect();
   db = result.db(databaseName);
-  collection = db.collection(collectionName);
-  const insertResult = await collection.insertOne(list);
+  collection = db.collection(colName);
+  const insertResult = await collection.insertOne(item);
   return insertResult;
 }
 
 // A function to retrieve event by ID from the database
-async function getListById(listId) {
+async function getEventById(eventId, colName) {
   let result = await client.connect();
   db = result.db(databaseName);
-  collection = db.collection(collectionName);
-  const list = await collection.findOne({ _id: new ObjectId(listId) });
-  return list;
+  collection = db.collection(colName);
+  const event = await collection.findOne({ _id: new ObjectId(eventId) });
+  return event;
 }
+
+// The call from the creatEvent to insert a new event into the database
+app.post('/newEvent', (req, res) => {
+  console.log('Entered /newEvent'); // for debug
+
+  collectionName = 'eventsInfo'
+  const newEvent = {
+    title: req.body.title,
+    date: req.body.date,
+    time: req.body.time,
+    place: req.body.place,
+    concept: req.body.concept,
+    number: req.body.number,
+  };
+  console.log(newEvent); // for debug
+
+  (async () => {
+    try {
+      const response = await insertItem(newEvent, collectionName);
+      console.log(response); // for debug
+      res.status(200).json(response); // Send the responses of the insertion
+    } catch (error) {
+      console.error(error); // for debug
+      res.status(500).json({ error: 'Failed to insert event' });
+    }
+  })();
+
+});
+
+// The call to get an event info based on an id from the database
+app.get('/eventInfo/:id', async (req, res) => {
+  console.log('Entered /eventInfo'); // for debug
+
+  collectionName = 'eventsInfo'
+  const eventId = req.params.id;
+  console.log(eventId); // for debug
+
+  try {
+    const event = await getEventById(eventId, collectionName);
+    console.log(event); // for debug
+    res.status(200).json(event);
+  } catch (error) {
+    console.error(error); // for debug
+    res.status(500).json({ error: 'Failed to retrieve event info' });
+  }
+});
+
+// The call from the website to insert a confirmation into the database
+app.post('/confirmation', (req, res) => {
+  console.log('Entered /confirmation'); // for debug
+
+  collectionName = 'confirmations'
+  const newconfirmation = {
+    eventID: req.body.eventID,
+    name: req.body.name,
+    option: req.body.option,
+  };
+  console.log(newconfirmation); // for debug
+
+  (async () => {
+    try {
+      const response = await insertItem(newconfirmation, collectionName);
+      console.log(response); // for debug
+      res.status(200).json({ message: 'Confirmation received!' }); // Send the responses of the insertion
+    } catch (error) {
+      console.error(error); // for debug
+      res.status(500).json({ error: 'Failed to insert confirmation' });
+    }
+  })();
+
+});
+
+
+app.get('/prompt', async (req, res) => {
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+      model: "gpt-3.5-turbo-0301",
+      messages: [
+        {
+          role: "user",  
+          content: `Hi, I'm planning a Italian Meal in a potluck style for 10 friends at My house. Can you create a list of things we need to bring please? Make sure the list contains appetizers, mains, sides, dessert, drinks and any other food or non-food items needed, not ingredients or partial dishes. Have only the names of things, have appropriate amounts and be thorough please. No intro and no outro or tips. Thank you! Please return this as a JSON file where each item has an amount and an item name.`
+        }
+      ]
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer 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`
+      }
+    });
+
+    let prompt = response.data.choices[0].message.content;
+    res.status(200).json({ prompt });
+
+  } catch (error) {
+    console.error('Error:', error);
+    console.error('Error details:', error.response.data);
+    res.status(500).json({ error: 'Failed to generate prompt' });
+}
+});
+
+app.post('/ingredients', async (req, res) => {
+  collectionName = 'Ingredients'
+  const prompt = req.body.prompt;
+
+  try {
+    const response = await insertItem({ content: prompt }, collectionName);
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to insert ingredient list' });
+  }
+});
 
 // The call from the group task list page to to get a list based on an id from the database
 app.get('/listInfo/:id', async (req, res) => {
@@ -82,11 +152,16 @@ app.get('/listInfo/:id', async (req, res) => {
   console.log(listId); // for debug
 
   try {
-    const list = await getListById(listId);
+    const list = await getItemById(listId, collectionName);
     console.log(list); // for debug
     res.status(200).json(list);
   } catch (error) {
     console.error(error); // for debug
     res.status(500).json({ error: 'Failed to retrieve ingredient list' });
   }
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
